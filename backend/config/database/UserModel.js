790bbe6a45b6eab31cca2032fb20ed49ccc86daa
Curvu <curvu6@gmail.com>
2023-04-14 23:57:12 +0100

    improvements in UserSchema (more) Student, School and IMT Models created

--- backend/config/database/UserModel.js
+++ backend/config/database/UserModel.js
@@ -1,13 +1,36 @@
 const mongoose = require('mongoose');
+const extendSchema = require('mongoose-extend-schema');
 
 mongoose.connect('mongodb://localhost:27017/shiftappens'); 
 
-// Schema for users
-const userSchema = mongoose.Schema({
-	username: String,
-	password: String
-})
+// login schema
+const UserSchema = mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	}
+});
 
-const UserModel = mongoose.model('User', userSchema);
+const StudentSchema = extendSchema(UserSchema, {
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	}
+});
 
-module.exports = UserModel;
+const SchoolSchema = extendSchema(UserSchema, {});
+
+const IMTSchema = UserSchema;
+
+const UserModel = mongoose.model('User', UserSchema);
+const StudentModel = mongoose.model('Student', StudentSchema);
+const SchoolModel = mongoose.model('School', SchoolSchema);
+const IMTModel = mongoose.model('IMT', IMTSchema);
+
+module.exports = { UserModel, StudentModel, SchoolModel, IMTModel };
