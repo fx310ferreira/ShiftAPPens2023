874540f6b5c3138c6736346c55011dc707fe24d5
Curvu <curvu6@gmail.com>
2023-04-15 17:14:26 +0100

    more models - imt - teacher

--- backend/config/database/UserModel.js
+++ backend/config/database/UserModel.js
@@ -5,32 +5,39 @@ mongoose.connect('mongodb://localhost:27017/shiftappens');
 
 // login schema
 const UserSchema = mongoose.Schema({
-	username: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	}
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true }
 });
 
 const StudentSchema = extendSchema(UserSchema, {
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	}
+	exams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Exam' }],
+	schedules: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Schedule' }],
 });
 
-const SchoolSchema = extendSchema(UserSchema, {});
+const TeacherSchema = mongoose.Schema({
+	email: { type: String, required: true, unique: true },
+	phone: { type: String, required: true },
+	exams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Exam' }],
+	schedules: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Schedule' }]
+});
 
-const IMTSchema = UserSchema;
+const ExaminerSchema = TeacherSchema;
 
-const UserModel = mongoose.model('User', UserSchema);
+const SchoolSchema = extendSchema(UserSchema, {
+	name: { type: String, required: true },
+	phone: { type: String, required: true },
+	teachers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Teachers' }]
+});
+
+const IMTSchema = extendSchema(UserSchema, {
+	phone: { type: String, required: true },
+	examiners: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Examiners' }]
+});
+
 const StudentModel = mongoose.model('Student', StudentSchema);
+const TeacherModel = mongoose.model('Teacher', TeacherSchema);
+const ExaminerModel = mongoose.model('Examiner', ExaminerSchema);
 const SchoolModel = mongoose.model('School', SchoolSchema);
 const IMTModel = mongoose.model('IMT', IMTSchema);
 
-module.exports = { UserModel, StudentModel, SchoolModel, IMTModel };
+module.exports = { StudentModel, TeacherModel, ExaminerModel, SchoolModel, IMTModel };
