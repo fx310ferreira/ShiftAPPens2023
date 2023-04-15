c6893cccb108c9cf232eb078b48f61cbb30b0ddc
Curvu <curvu6@gmail.com>
2023-04-15 17:13:44 +0100

    more endpoints - /register/teacher - /register/examiner - /add/schedule/:id - /add/exam - /exams

--- backend/routes/file_links.js
+++ backend/routes/file_links.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 require("../config/passport");
 const express = require("express");
-const { UserModel, StudentModel, SchoolModel, IMTModel } = require("../config/database/UserModel");
+const { StudentModel, TeacherModel, ExaminerModel, SchoolModel, IMTModel } = require("../config/database/UserModel");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
 
@@ -73,6 +73,40 @@ router.post('/register/student', (req, res) => {
   });
 });
 
+// post request to register teacher
+router.post('/register/teacher', (req, res) => {
+  TeacherModel.create(req.body)
+  .then((user) => {
+    return res.status(200).send({
+      sucess: true,
+      message: "Teacher created!",
+    });
+  })
+  .catch((err) => {
+    return res.status(401).send({
+      sucess: false,
+      message: "ERROR: Creating teacher!",
+    });
+  });
+});
+
+// post request to register examiner
+router.post('/register/examiner', (req, res) => {
+  ExaminerModel.create(req.body)
+  .then((user) => {
+    return res.status(200).send({
+      sucess: true,
+      message: "Examiner created!",
+    });
+  })
+  .catch((err) => {
+    return res.status(401).send({
+      sucess: false,
+      message: "ERROR: Creating examiner!",
+    });
+  });
+});
+
 // post request to register school
 router.post('/register/school', (req, res) => {
   SchoolModel.create(req.body)
@@ -89,6 +123,62 @@ router.post('/register/school', (req, res) => {
       message: "ERROR: Creating school!",
     });
   });
+});
+
+// post request to add schedule to student/teacher/examiner
+const addSchedule = (req, res, user) => {
+  if (!user) {
+    return res.status(401).send({
+      sucess: false,
+      message: "User not found!",
+    });
+  };
+
+  user.schedules.push(req.body);
+  user.save();
+
+  return res.status(200).send({
+    sucess: true,
+    message: "Schedule added!",
+  });
+}
+
+router.post('/add/schedule/:id', (req, res) => { // look for user
+  StudentModel.findById(req.param.id).then((user) => addSchedule(req, res, user));
+  TeacherModel.findById(req.param.id).then((user) => addSchedule(req, res, user));
+  ExaminerModel.findById(req.param.id).then((user) => addSchedule(req, res, user));
+});
+
+// post request to add exam to student, examiner (and teacher [optional])
+router.post('/add/exam/', async (req, res) => {
+  const user = await StudentModel.findById(req.body.studentId);
+  const examiner = await ExaminerModel.findById(req.body.examinerId);
+  const teacher = await TeacherModel.findById(req.body.teacherId);
+  if (!user || !examiner) {
+    return res.status(401).send({
+      sucess: false,
+      message: "Invalid request!",
+    });
+  };
+
+  user.exams.push(req.body);
+  examiner.exams.push(req.body);
+  if (teacher) {
+    teacher.exams.push(req.body);
+    teacher.save();
+  }
+  user.save();
+  examiner.save();
+
+  return res.status(200).send({
+    sucess: true,
+    message: "Exam added!",
+  });
+});
+
+// get all exams using imt token
+router.get('/exams', passport.authenticate('jwt', { session: false }), (req, res) => {
+  console.log(req.user);
 });
 
 module.exports = router;
