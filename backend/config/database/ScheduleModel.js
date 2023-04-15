ec7950e66061ce9ec52b246d5af912346a4f1d62
Curvu <curvu6@gmail.com>
2023-04-16 00:24:29 +0100

    minor fixes

--- backend/config/database/ScheduleModel.js
+++ backend/config/database/ScheduleModel.js
@@ -5,7 +5,7 @@ mongoose.connect('mongodb://localhost:27017/shiftappens');
 const ScheduleSchema = mongoose.Schema({
   start: { type: Date, required: true },
   end: { type: Date, required: true },
-  status: { type: String, enum: ['disponivel', 'conducao', 'codigo'], required: true },
+  status: { type: String, enum: ['available', 'drive', 'code'], required: true },
 });
 
 const ScheduleModel = mongoose.model('Schedule', ScheduleSchema);
